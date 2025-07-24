0bccaeb06cf3836bd0d56fa07cf8caa61688624f
Grimcat07 <166890725+Grimcat07@users.noreply.github.com>
2025-07-24 19:25:44 +0530

    added hybrid drop down menu

---
+++ hybrid/script.js
@@ -0,0 +1,19 @@
+const container=document.querySelectorAll(".container")
+container.forEach((contain)=>{
+    let isClicked=false
+    const trigger=contain.querySelector(".trigger")
+    const content=contain.querySelector(".content")
+content.style.display="none"
+trigger.addEventListener("click",()=>{
+    isClicked=!isClicked
+    content.style.display= isClicked? "block":"none"
+})
+trigger.addEventListener("mouseenter",()=>
+{
+    if(!isClicked)content.style.display="block"
+})
+trigger.addEventListener("mouseleave",()=>{
+    if(!isClicked)content.style.display="none"
+})
+})
+
