83a834685f84000b24cf0f5920202580d384290a
Grimcat07 <166890725+Grimcat07@users.noreply.github.com>
2025-07-24 19:34:57 +0530

    added some basic styles

--- click/script.js
+++ click/script.js
@@ -1,10 +1,11 @@
-const container=document.querySelectorAll(".container")
-container.forEach((contain)=>{
-    const trigger=contain.querySelector(".trigger")
-    const content=contain.querySelector(".content")
+const container = document.querySelectorAll(".container");
+container.forEach((contain) => {
+  const trigger = contain.querySelector(".trigger");
+  const content = contain.querySelector(".content");
 
-trigger.addEventListener("click",()=>
-{
-    getComputedStyle(content).display=="none"? content.style.display="block":content.style.display="none"
-})
-})
+  trigger.addEventListener("click", () => {
+    getComputedStyle(content).display == "none"
+      ? (content.style.display = "block")
+      : (content.style.display = "none");
+  });
+});
--- hover/script.js
+++ hover/script.js
@@ -1,15 +1,12 @@
-const container=document.querySelectorAll(".container")
-container.forEach((contain)=>{
-    
-    const trigger=contain.querySelector(".trigger")
-    const content=contain.querySelector(".content")
-content.style.display="none"
-trigger.addEventListener("mouseenter",()=>
-{
-    content.style.display="block"
-})
-trigger.addEventListener("mouseleave",()=>{
-    content.style.display="none"
-})
-})
-
+const container = document.querySelectorAll(".container");
+container.forEach((contain) => {
+  const trigger = contain.querySelector(".trigger");
+  const content = contain.querySelector(".content");
+  content.style.display = "none";
+  trigger.addEventListener("mouseenter", () => {
+    content.style.display = "block";
+  });
+  trigger.addEventListener("mouseleave", () => {
+    content.style.display = "none";
+  });
+});
